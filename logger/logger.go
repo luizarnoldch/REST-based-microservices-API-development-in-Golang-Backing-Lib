733191adfed41b758b27a5ae210dfcddbe0ffda7
arnold <67833474+luizarnoldch@users.noreply.github.com>
2022-03-28 14:04:31 -0500

    bancking lib: v0.1.0

--- logger/logger.go
+++ logger/logger.go
@@ -13,26 +13,30 @@ func init() {
 	config := zap.NewProductionConfig()
 
 	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.TimeKey= "timestamp"
+	encoderConfig.TimeKey = "timestamp"
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.StacktraceKey = ""
 	config.EncoderConfig = encoderConfig
 
 	log, err = config.Build(zap.AddCallerSkip(1))
-	//log, err = zap.NewProduction(zap.AddCallerSkip(1))
+
 	if err != nil {
 		panic(err)
 	}
 }
 
-func Info(message string, fields ...zap.Field){
-	log.Info(message,fields...)
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
 }
 
-func Debug(message string, fields ...zap.Field){
-	log.Debug(message,fields...)
+func Fatal(message string, fields ...zap.Field) {
+	log.Fatal(message, fields...)
 }
 
-func Error(message string, fields ...zap.Field){
-	log.Error(message,fields...)
+func Debug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
 }
